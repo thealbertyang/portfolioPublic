2f163ff3b2f44154219ce9fcdaeb0bf2b2555c07
Albert Yang <albert@pushpress.com>
2019-10-17 22:34:22 -0700

    Resolved undefined error

--- portfolio/components/page/Section.js
+++ portfolio/components/page/Section.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Section = ({ name, fluid, backgroundImage, backgroundColor, backgroundSize, backgroundPosition, height, className, children }) => {
-    console.log(typeofbackgroundImage);
+    console.log(backgroundImage);
     console.log(backgroundImage);
     return (
         <div 
@@ -13,7 +13,7 @@ const Section = ({ name, fluid, backgroundImage, backgroundColor, backgroundSize
             `} 
             style={{
                 backgroundColor: typeof backgroundColor !== undefined && backgroundImage !== null ? backgroundColor : null,
-                ...(typeof backgroundImage !== undefined && backgroundImage !== null) && { backgroundImage: `url( ${backgroundImage} )`},
+                backgroundImage: typeof backgroundImage !== undefined && backgroundImage !== null ? `url( ${backgroundImage} )` : null,
                 backgroundSize:  typeof backgroundSize !== undefined ? backgroundSize : 'center center',
                 backgroundPosition: typeof backgroundPosition !== undefined ? backgroundPosition : 'cover',
                 backgroundRepeat: 'no-repeat',
