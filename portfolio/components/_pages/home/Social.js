5a9a14b5e1e2cef2200c2ec88757ff1430a81a95
Albert <thealbertyang@gmail.com>
2020-08-04 00:16:38 -0700

    Social update

--- portfolio/components/_pages/home/Social.js
+++ portfolio/components/_pages/home/Social.js
@@ -3,13 +3,18 @@ import Section from '../../page/Section'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
+import config from '../../../config.js'
 
 const Social = () =>
 <Section fluid className={`d-flex p-5`} name={`social`} style={{ minHeight: 'calc(100vh + 50px)' }} background={`linear-gradient(180deg, #151124 0%, #31169D 100%)`}>
     <div className="row flex-fill">
         <div className='col-md-12 d-flex align-items-center justify-content-center'>
-          <FontAwesomeIcon icon={faGithubAlt} className="mx-5 text-white" size="9x" />
-          <FontAwesomeIcon icon={faLinkedin} className="mx-5 text-white" size="9x" />
+          <a href={config.urls.github} target="_blank">
+            <FontAwesomeIcon icon={faGithubAlt} className="mx-5 text-white" size="9x" />
+          </a>
+          <a href={config.urls.linkedin} target="_blank">
+            <FontAwesomeIcon icon={faLinkedin} className="mx-5 text-white" size="9x" />
+          </a>
         </div>
     </div>
 </Section>
---
+++ portfolio/config.js
@@ -0,0 +1,8 @@
+const config = {
+    urls: {
+        github: 'https://github.com/thealbertyang',
+        linkedin: 'https://linkedin.com/in/thealbertyang'
+    }
+}
+
+export default config
