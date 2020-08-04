d6ed4ac647fdf8da6d3d00b33fa7fe42de32ebd8
Albert <thealbertyang@gmail.com>
2020-08-04 05:56:07 -0700

    Update Social

--- portfolio/components/_pages/home/Social.js
+++ portfolio/components/_pages/home/Social.js
@@ -5,16 +5,33 @@ import { faGithubAlt, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 import config from '../../../config.js'
 
+const iconsMap = [
+  {
+    url: config.urls.github,
+    icon: faGithubAlt 
+  },
+  {
+    url: config.urls.linkedin,
+    icon: faLinkedin 
+  },
+]
+
+const Icon = ({ url, icon }) =>
+  <a href={url} target="_blank" className="mx-5">
+    <FontAwesomeIcon icon={icon} className="text-white" size="9x" />
+  </a>
+
 const Social = () =>
 <Section fluid className={`d-flex p-5`} name={`social`} style={{ minHeight: 'calc(100vh + 50px)' }} background={`linear-gradient(180deg, #151124 0%, #31169D 100%)`}>
     <div className="row flex-fill">
         <div className='col-md-12 d-flex align-items-center justify-content-center'>
-          <a href={config.urls.github} target="_blank">
-            <FontAwesomeIcon icon={faGithubAlt} className="mx-5 text-white" size="9x" />
-          </a>
-          <a href={config.urls.linkedin} target="_blank">
-            <FontAwesomeIcon icon={faLinkedin} className="mx-5 text-white" size="9x" />
-          </a>
+          {
+            iconsMap && iconsMap.map(item => 
+              <Icon 
+                {...item}
+              />
+            )
+          }
         </div>
     </div>
 </Section>
